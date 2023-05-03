9061da1072ba53ada9935ad20e02f7e679e5b2d9
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-05-03 14:35:32 +0700

    update event expired

--- assets/js/detail-event.js
+++ assets/js/detail-event.js
@@ -1,67 +1,88 @@
-let id = Number.parseInt((new Map(location.search.replace("?", "").split("&").map(e => e.split("=")))).get("id")) ?? 0
+let id =
+  Number.parseInt(
+    new Map(
+      location.search
+        .replace("?", "")
+        .split("&")
+        .map((e) => e.split("="))
+    ).get("id")
+  ) ?? 0;
 
 if (id == 0 || Number.isNaN(id)) {
-     document.getElementById('output-detail').innerHTML = 'Data kosong'
+  document.getElementById("output-detail").innerHTML = "Data kosong";
 } else {
-     apiEvent()
+  apiEvent();
 }
 
 function apiEvent() {
-     fetch('https://644dfece4e86e9a4d8ef004c.mockapi.io/detail-events')
-          .then(function (res) {
-               return res.json()
-          })
-          .then(function (data) {
-               let output = ''
+  fetch("https://644dfece4e86e9a4d8ef004c.mockapi.io/detail-events")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let output = "";
 
-               data.forEach(function (el) {
-                    if (el.id == id) {
-                         output += `
-                              <div id="output-detail" class="container my-5 d-flex justify-content-center align-content-center">
+      data.forEach(function (el) {
+        if (el.id == id) {
+          output += `
+               <div id="output-detail" class="container my-5 d-flex flex-sm-column justify-content-justify-content-center align-content-center flex-lg-row">
                     <div class="row">
-                         <div class="col-md-6">
-                              <img src="${el.gambar}" class="img-responsive mt-5 w-100"
-                                   alt="ilustrasi mencegah banjir">
-                              <div class="activity-detail mt-2">
-                                   <h3>Activity Detail</h3>
-                                   <p>
-                                        ${el.detail}
-                                   </p>
-                              </div>
-                         </div>
-     
-                         <div class="col-md-6 mt-5">
-                              <div class="d-flex justify-content-center align-content-center">
-                                   <div class="card card-detail" style="width: 30rem;">
-                                        <div class="card-body">
-                                             <h5 class="fw-bold">
-                                                  ${el.judul}
-                                             </h5>
-                                        </div>
-                                        <ul class="list-group list-group-item">
-                                             <li class="list-group-item"><i class="fa-solid fa-calendar"></i>&nbsp;${el.tanggal}
-                                             </li>
-                                             <li class="list-group-item"><i class="fa-solid fa-clock"></i>&nbsp;${el.waktu}
-                                             </li>
-                                             <li class="list-group-item"><i class="fa-solid fa-ticket"></i>&nbsp;${el.tiket}</li>
-                                             <li class="list-group-item"><i
-                                                       class="fa-solid fa-location-dot"></i>&nbsp;${el.lokasi}</li>
-                                        </ul>
-                                        <div class="d-grid p-3">
-                                             <a href="" class="btn btn-primary">Daftar Event</a>
-                                        </div>
+                         <div class="col-lg-9 col-md-12">
+                              <div class="img-detail mt-5">
+                                   <img src="${el.gambar}" class="img-responsive img-fluid w-100"
+                                        alt="ilustrasi mencegah banjir">
+                                   <div class="activity-detail mt-2">
+                                        <h4>Activity Detail</h4>
+                                        <p class="text-detail">
+                                             ${el.detail}
+                                        </p>
                                    </div>
                               </div>
                          </div>
                     </div>
-                    `
-                    }
-               })
-               document.getElementById('output-detail').innerHTML = output
-          })
-          .catch(function (err) {
-               console.log(err)
-          })
+          
+<div class="row">
+     <div class="col-md-12 col-lg-3">
+     <div class="card mt-5 mx-md-auto" style="width: 25rem; height: 28rem;">
+          <div class="card-body">
+          <h4 class="card-title mb-4 fw-bold">${el.judul}</h4>
+          <div class="date-detail mb-4">
+               <i class="fa-solid fa-calendar"></i>
+               <p class="d-inline text-muted ms-1">${el.tanggal}</p>
+          </div>
+          <div class="time-detail mb-4">
+               <i class="fa-solid fa-clock"></i>
+               <p class="d-inline text-muted ms-1">${el.waktu}</p>
+          </div>
+          <div class="ticket-detail mb-4">
+               <i class="fa-solid fa-ticket"></i>
+               <p class="d-inline text-muted ms-1">${el.tiket}</p>
+          </div>
+          <div class="location-detail mb-4">
+               <i class="fa-solid fa-location-dot"></i>
+               <p class="d-inline text-muted ms-1">${el.lokasi}</p>
+          </div>
+          <div class="registration-detail mb-4">
+               <i class="fa-solid fa-address-card"></i>
+               <p class="d-inline text-muted ms-1">${el.pendaftaran}</p>
+          </div>
+          <div class="d-grid mt-3 btn-detail-event">
+               <a href="../event-success.html" class="btn btn-primary">Daftar Event</a>
+          </div>
+          </div>
+     </div>
+     </div>
+     </div>
+
+     </div>
+                         `;
+        }
+      });
+      document.getElementById("output-detail").innerHTML = output;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
 
-apiEvent()
+apiEvent();
